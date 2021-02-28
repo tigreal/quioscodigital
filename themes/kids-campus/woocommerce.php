c8d51930b2a9fae5133f714960e901af694ad493
tigreal <fosmaralvarez@gmail.com>
2021-02-28 10:06:07 -0400

    solo_temas_plugins_y_uploads

---
+++ themes/kids-campus/woocommerce.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<div class="container">
+    	<div id="kc_content_wrap">
+			<?php woocommerce_content(); ?>
+		</div><!-- kc_content_wrap -->
+</div><!-- container -->     
+<?php get_footer(); ?>
